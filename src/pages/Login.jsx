5c8d2c4fbe18f62671dbad724778f3f1ee38cf1c
Alvin Reihansyah Makarim <reihansyah6@gmail.com>
2026-04-20 00:24:10 +0700

    feat: implement authentication logic and state

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,35 @@
-// Login.jsx
+import { useState } from 'react';
+import { useAuth } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
 export default function Login() {
+  const [name, setName] = useState('');
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    login({ name });
+
+    navigate('/');
+  };
+
   return (
-    <div className="bg-green-500 text-white text-3xl p-10">Login Page</div>
+    <div className="flex justify-center items-center h-screen">
+      <form onSubmit={handleLogin} className="flex flex-col gap-4 w-80">
+        <h1 className="text-2xl font-bold">Login</h1>
+
+        <input
+          type="text"
+          placeholder="Nama"
+          className="border p-2 rounded"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <button className="bg-green-500 text-white p-2 rounded">Login</button>
+      </form>
+    </div>
   );
 }
